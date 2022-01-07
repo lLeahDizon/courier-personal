c66cb9987b5e021c5e7c83d9994b8df4bd73ad5a
Lemon <516330797@qq.com>
2022-01-08 00:09:10 +0800

    feature: 完成电话下单

--- src/components/Home/Phone.vue
+++ src/components/Home/Phone.vue
@@ -1,11 +1,30 @@
 <template>
-  <div></div>
+  <div class="phone">
+    <img src="@/assets/icons/phone-order.png" alt="" @click="clickPhone">
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    clickPhone() {
+      // todo 替换电话号码
+      window.location.href = 'tel:15779500011'
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.phone {
+  padding-top: 108px;
+  padding-bottom: 50px;
+  display: flex;
+  justify-content: center;
 
+  img {
+    width: 340px;
+    height: 340px;
+  }
+}
 </style>
