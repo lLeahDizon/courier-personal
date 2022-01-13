5b843e494113a4530a21f898cdcc91bc3c32522d
lLeahDizon <516330797@qq.com>
2022-01-13 18:17:21 +0800

    feature: 完成认证弹窗样式

--- src/components/Home/ModalCertification.vue
+++ src/components/Home/ModalCertification.vue
@@ -1,6 +1,9 @@
 <template>
   <van-popup v-model="visible" class="modal-wrapper" round :close-on-click-overlay="false" get-container="#app">
-    123123
+    <Icon name="certification-close" @click="onClose"/>
+    <p class="title">实名认证</p>
+    <p class="content"><span>为营造安全优质的服务和资金安全</span><span>请先完成</span></p>
+    <button class="btn" @click="onCertificate">去认证</button>
   </van-popup>
 </template>
 
@@ -21,6 +24,9 @@ export default {
   methods: {
     onClose() {
       this.$emit('update:show', false)
+    },
+    onCertificate () {
+      this.onClose()
     }
   }
 }
@@ -28,7 +34,40 @@ export default {
 
 <style lang="scss" scoped>
 .modal-wrapper {
-  width: 650px;
-  height: 932px;
+  text-align: center;
+  width: 580px;
+  color: #333333;
+  padding: 36px 80px 40px;
+
+  .icon {
+    position: absolute;
+    font-size: 30px;
+    right: 24px;
+    top: 24px;
+  }
+
+  > .title {
+    font-size: 36px;
+    line-height: 50px;
+    font-weight: bold;
+  }
+
+  > .content {
+    display: flex;
+    flex-direction: column;
+    padding: 40px 0 60px;
+    font-size: 28px;
+    line-height: 40px;
+  }
+
+  > .btn {
+    background: #12A0FF;
+    border-radius: 50px;
+    padding: 20px 0;
+    width: 100%;
+    font-size: 36px;
+    line-height: 44px;
+    color: white;
+  }
 }
 </style>
