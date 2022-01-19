6a8db0568f812966cc858953d68ce3bda4125c0b
lLeahDizon <516330797@qq.com>
2022-01-20 02:16:03 +0800

    feature: 完成物品信息页 UI

---
+++ src/components/InfoItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="InfoItem">
+    <div class="item">
+      <span :class="{isRequired}">{{ title }}</span>
+      <div class="right">
+        <div :class="{'placeholder':!content}">{{ content || placeholder }}</div>
+        <Icon name="my-right"/>
+      </div>
+    </div>
+    <div v-if="desc" class="desc">{{ desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoItem',
+  props: {
+    'isRequired': {
+      type: Boolean,
+      default: true
+    },
+    'title': {
+      type: String,
+      required: true
+    },
+    'placeholder': {
+      type: String,
+      default: ''
+    },
+    'content': {
+      type: String,
+      default: ''
+    },
+    'desc': {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.InfoItem {
+  padding: 30px 0;
+  border-bottom: 1px solid #e6e6e6;
+
+  > .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 32px;
+    line-height: 44px;
+    color: #333333;
+
+    > .isRequired:before {
+      content: '*';
+      color: #f12323;
+      padding-right: 12px;
+    }
+
+    > .right {
+      display: flex;
+      align-items: center;
+
+      > .placeholder {
+        color: #aaaaaa;
+      }
+
+      .icon {
+        margin-left: 16px;
+        width: 14px;
+        height: 24px;
+      }
+    }
+  }
+
+  > .desc {
+    font-size: 26px;
+    line-height: 36px;
+    color: #333333;
+  }
+}
+</style>
--- src/views/GoodsInfo.vue
+++ src/views/GoodsInfo.vue
@@ -1,29 +1,80 @@
 <template>
   <div class="GoodsInfo-wrapper">
-    <h1 class="title">发件距离</h1>
-    <p>根据您当前的收发件信息，距离约为 <span>150km</span></p>
-    <p></p>
+    <header>
+      <h1 class="title">发件距离</h1>
+      <p class="desc">根据您当前的收发件信息，距离约为 <span class="number">150km</span></p>
+      <p class="desc time">预计 <span class="number">1-6小时内</span> 送达</p>
+    </header>
+    <InfoItem title="物品信息" placeholder="请选择物品信息" :content="123"/>
+    <InfoItem title="期望取件时间" placeholder="请选择取件时间" :content="123"/>
+    <InfoItem title="期望送达时间" placeholder="请选择送达时间" :content="123"/>
+    <InfoItem :is-required="false" title="物品报价" placeholder="可选择报价赔付"/>
+    <div class="btn-wrapper">
+      <button class="btn" @click="onSubmit">确认发布订单</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import InfoItem from '@/components/InfoItem'
+
+export default {
+  components: {InfoItem},
+  methods: {
+    onSubmit() {}
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .GoodsInfo-wrapper {
-  padding: 40px 30px;
+  padding: 0 30px;
 
-  > .title {
-    font-size: 32px;
-    color: #333333;
-    line-height: 44px;
+  header {
+    padding: 40px 0;
+    border-bottom: 1px solid #e6e6e6;
+
+    > .title {
+      font-size: 32px;
+      color: #333333;
+      line-height: 44px;
+      padding-bottom: 16px;
+    }
+
+    > .desc {
+      font-size: 28px;
+      color: #666666;
+      line-height: 40px;
+
+      &.time {
+        padding-top: 10px;
+      }
+
+      > .number {
+        font-size: 36px;
+        line-height: 40px;
+        color: #12a0ff;
+      }
+    }
+
   }
 
-  > .desc {
-    font-size: 28px;
-    color: #666666;
-    line-height: 40px;
+  .btn {
+    width: 100%;
+    background: #12a0ff;
+    padding: 20px 0;
+    border-radius: 50px;
+    font-size: 36px;
+    line-height: 44px;
+    color: white;
+
+    &-wrapper {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      padding: 0 30px 40px;
+    }
   }
 }
 </style>
